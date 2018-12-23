104fcd7f7ceb5bedd441fcee1643d902af459ea1
asus <975254814@qq.com>
2018-12-23 16:07:05 +0800

    前台登陆注册

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,9 +1,12 @@
 <?php
 namespace app\index\controller;
 
-use app\admin\model\Game;
-use think\Controller;
 use app\admin\model\Information;
+use app\admin\model\Game;
+use app\admin\model\User;
+use think\Controller;
+use think\Request;
+use think\Cookie;
 
 class Index extends Controller
 {
@@ -77,6 +80,84 @@ class Index extends Controller
         $spggames = $game->where("gameType = 'SPG'")->limit(5)->select();
         $this->assign('spggames',$spggames);
 
+        return view();
+    }
+    public function login()
+    {
+        $request = Request::instance();
+        
+        if(null!==$request->cookie('userID')){
+            $this->redirect("index/index");
+        }
+        $user=new User();
+        if($request->has("userName","post")&&$request->has("password","post")){
+            $userName=$request->post("userName");
+            $password=$request->post("password");
+            $userInfo=$user->where("userName",$userName)->find();
+            if($userInfo!==null&&$password!==''){
+                if($userInfo["password"]==$password){
+                    Cookie::set("userID",$userInfo["userID"]);
+                    $this->redirect("index/index");
+                }
+            }
+            else{
+                echo "<script>
+                     alert('登陆失败！');
+                    </script>";
+            }
+        }
+        return view();
+    }
+    public function register()
+    {
+        $request = Request::instance();
+        $user=new User();
+
+        if($request->has("userName","post")&&$request->has("password","post")){
+            $userName=$request->post("userName");
+            $password=$request->post("password");
+            $password1=$request->post("password1");
+            $img=$request->file("userImg");
+            $userInfo=$user->where("userName",$userName)->find();
+            if($userName!==''){
+                if($password!==''){
+                    if(null!=$userInfo){
+                        echo "<script>
+                     alert('该用户名被占用！');
+                    </script>";
+                    }else if($password!=$password1){
+                        echo "<script>
+                     alert('请输入相同的密码！');
+                    </script>";
+                    }else{
+                        $userID=$user->max("userID")+1;
+                        $user->addUser($userID, $userName, $password, 1, 1);
+                        $imgInfo = $img->move(ROOT_PATH . 'public' . DS . 'uploads' . DS . 'userImg',$userID);
+                        echo "<script>
+                     alert('注册成功！');
+                    </script>";
+                        Cookie::set("userID",$userInfo["userID"]);
+                        $this->redirect("index/login");
+                    }
+                }else{
+                    echo "<script>
+                     alert('密码不能为空！');
+                    </script>";
+                }
+            }else{
+                echo "<script>
+                     alert('用户名不能为空！');
+                    </script>";
+            }
+        }
+        else{
+            echo "<script>
+                     alert('".$userName=$request->post("userName")."');
+                    </script>";
+        }
+        return view();
+    }
+    public function information_page(){
         return view();
     }
 }
